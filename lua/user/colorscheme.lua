151e46664dd393aede74605214325ac7d88cadda
Mike <mike@Mikes-MBP-2.broadband>
2022-08-07 12:04:07 +0100

    cleanup cmp

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -20,6 +20,5 @@ vim.g.tokyonight_dark_float = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	return
 end
