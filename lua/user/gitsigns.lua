326a7b417002ef60be53c4fa9cdd42c5105978e7
Mike <mike@Mikes-MBP-2.broadband>
2022-08-07 12:19:44 +0100

    cleanup gitsigns

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -45,7 +45,7 @@ gitsigns.setup({
 	yadm = {
 		enable = false,
 	},
-	-- navigation
+	-- Navigation
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
 
@@ -55,7 +55,6 @@ gitsigns.setup({
 			vim.keymap.set(mode, l, r, opts)
 		end
 
-		-- Navigation
 		map("n", "<leader>gj", function()
 			if vim.wo.diff then
 				return "]c"
@@ -75,23 +74,6 @@ gitsigns.setup({
 			end)
 			return "<Ignore>"
 		end, { expr = true })
-
-		-- Actions
-		-- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-		-- map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-		-- map("n", "<leader>hS", gs.stage_buffer)
-		-- map("n", "<leader>hu", gs.undo_stage_hunk)
-		-- map("n", "<leader>hR", gs.reset_buffer)
-		-- map("n", "<leader>hp", gs.preview_hunk)
-		-- map("n", "<leader>hb", function()
-		-- 	gs.blame_line({ full = true })
-		-- end)
-		-- map("n", "<leader>tb", gs.toggle_current_line_blame)
-		-- map("n", "<leader>hd", gs.diffthis)
-		-- map("n", "<leader>hD", function()
-		-- 	gs.diffthis("~")
-		-- end)
-		-- map("n", "<leader>td", gs.toggle_deleted)
 
 		-- Text object
 		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
