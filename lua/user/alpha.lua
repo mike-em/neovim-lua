b021dcb752db0208aa40c6f47772621ddc6a4e28
Mike <mike@Mikes-MBP-2.broadband>
2022-08-07 12:14:07 +0100

    cleanup alpha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -1,43 +1,34 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-  return
+	return
 end
 
-local dashboard = require "alpha.themes.dashboard"
+local dashboard = require("alpha.themes.dashboard")
 
 local function button(sc, txt, keybind, keybind_opts)
-  local b = dashboard.button(sc, txt, keybind, keybind_opts)
-  b.opts.hl_shortcut = "Macro"
-  return b
+	local b = dashboard.button(sc, txt, keybind, keybind_opts)
+	b.opts.hl_shortcut = "Macro"
+	return b
 end
 
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+	[[                               __                ]],
+	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-  button("f", " Find file", ":Telescope find_files <CR>"),
-  button("t", " Find text", ":Telescope live_grep <CR>"),
-  button("r", " Recent files", ":Telescope oldfiles <CR>"),
-  button("e", " New file", ":ene <BAR> startinsert <CR>"),
-  -- button("p", " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-  -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
-  -- button("s", " Find Session", ":SearchSession<CR>"),
-  button("c", " Config", ":e ~/.config/nvim/init.lua <CR>"),
-  -- button("u", " Update", ":PackerSync<CR>"),
-  button("q", " Quit", ":qa<CR>"),
+	button("f", " Find file", ":Telescope find_files <CR>"),
+	button("t", " Find text", ":Telescope live_grep <CR>"),
+	button("r", " Recent files", ":Telescope oldfiles <CR>"),
+	button("e", " New file", ":ene <BAR> startinsert <CR>"),
+	button("c", " Config", ":e ~/.config/nvim/init.lua <CR>"),
+	button("q", " Quit", ":qa<CR>"),
 }
 local function footer()
-  -- NOTE: requires the fortune-mod package to work
-  -- local handle = io.popen("fortune")
-  -- local fortune = handle:read("*a")
-  -- handle:close()
-  -- return fortune
-  return "mikem.io"
+	return "mikem.io"
 end
 
 dashboard.section.footer.val = footer()
@@ -47,5 +38,4 @@ dashboard.section.buttons.opts.hl = "Macro"
 dashboard.section.footer.opts.hl = "Type"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
