703196944bc18bc3556c796c95f7314ee4485288
Michael Mikucki <michael@epoppay.com>
2023-09-07 10:55:13 +0100

    issue with vim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,7 +90,6 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim", commit = "8223c970677e4d88c9b6b6d81bda23daf11062bb" })
-
 	-- Utility
 	use({ "moll/vim-bbye", commit = "25ef93ac5a87526111f43e5110675032dbcacf56" })
 	use({ "lewis6991/impatient.nvim", commit = "4ccbe749ce439fa25d387d459e8c339131cc5d1f" })
